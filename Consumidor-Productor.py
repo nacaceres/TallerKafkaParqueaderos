a6ccd62f8dd4ec75312d5133837114f5678bfecd
nacaceres <35931488+nacaceres@users.noreply.github.com>
2018-09-11 22:29:34 -0500

    Update and rename Consumidor - Productor.py to Consumidor-Productor.py

--- Consumidor-Productor.py
+++ Consumidor-Productor.py
@@ -3,11 +3,11 @@ from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-consumer = KafkaConsumer(bootstrap_servers=[172.24.41.216:8081],
+consumer = KafkaConsumer(bootstrap_servers=['172.24.41.216:8081'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe(pattern='Disponibilidad')
 
-producer = KafkaProducer(bootstrap_servers=[172.24.41.216:8081], 
+producer = KafkaProducer(bootstrap_servers=['172.24.41.216:8081'], 
              value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 temp_list1 = 'El parqueadero con nombre: '
@@ -19,9 +19,9 @@ for message in consumer:
     temp_list1+=(message.value[“disponibles”])
     temp_list1+=' parqueaderos disponibles y tiene  '
     temp_list1+=(message.value['place'] == "ocupadosClientesNidoo")
-   temp_list1+='  ocupados por clients nidoo en la hora: '
+    temp_list1+='  ocupados por clients nidoo en la hora: '
     temp_list1+=(message.value['time'])
 
-      producer.send('clientes', {'Mensaje':temp_list1})
+      producer.send('Clientes', {'Mensaje':temp_list1})
       producer.flush()
       temp_list1 = 'El parqueadero con nombre: '
