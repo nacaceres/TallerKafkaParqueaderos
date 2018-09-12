a2ba3943b551db994986cda2fc5a5c329b684690
nacaceres <35931488+nacaceres@users.noreply.github.com>
2018-09-11 22:29:37 -0500

    Update productor.py

--- productor.py
+++ productor.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-producer = KafkaProducer(bootstrap_servers=[172.24.41.216:8081], 
+producer = KafkaProducer(bootstrap_servers=['172.24.41.216:8081'], 
 						 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 while True:
